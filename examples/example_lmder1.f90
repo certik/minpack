409ba02a107f47ee835f3976952bbc64dd46de8a
Ondřej Čertík <ondrej@certik.us>
2023-01-27 20:58:27 -0700

    Refactor example_lmder1(), remove closures
    Add tests.

--- examples/example_lmder1.f90
+++ examples/example_lmder1.f90
@@ -1,13 +1,12 @@
 module testmod_der1
+use minpack, only: chkder
 implicit none
-private
-public fcn, dp
 
 integer, parameter :: dp=kind(0d0)
 
 contains
 
-subroutine fcn(m, n, x, fvec, fjac, ldfjac, iflag)
+subroutine fcn2(m, n, x, fvec, fjac, ldfjac, iflag)
 integer, intent(in) :: m, n, ldfjac, iflag
 real(dp), intent(in) :: x(n)
 real(dp), intent(out) :: fvec(m), fjac(ldfjac, n)
@@ -39,30 +38,49 @@ else
 end if
 end subroutine
 
+subroutine check_deriv(x, fvec, fjac)
+real(dp), intent(in) :: x(:)
+real(dp), intent(out) :: fvec(:), fjac(:,:)
+real(dp) :: xp(size(x)), fvecp(size(fvec)), err(size(fvec))
+integer :: i
+call chkder(size(fvec), size(x), x, fvec, fjac, size(fjac, 1), xp, fvecp, &
+    1, err)
+call fcn2(size(fvec), size(x), x, fvec, fjac, size(fjac, 1), 1)
+call fcn2(size(fvec), size(x), x, fvec, fjac, size(fjac, 1), 2)
+call fcn2(size(fvec), size(x), xp, fvecp, fjac, size(fjac, 1), 1)
+call chkder(size(fvec), size(x), x, fvec, fjac, size(fjac, 1), xp, fvecp, &
+    2, err)
+print *, "Derivatives check (1.0 is correct, 0.0 is incorrect):"
+print *, err
+do i = 1, size(err)
+    if (abs(err(i) - 1) > 1e-15_dp) error stop
+end do
+end subroutine
+
 end module
 
 
 program example_lmder1
 use minpack, only: enorm, lmder1, chkder
-use testmod_der1, only: dp, fcn
+use testmod_der1, only: dp, fcn2, check_deriv
 implicit none
 
 integer :: info
-real(dp) :: tol, x(3), fvec(15), fjac(size(fvec), size(x))
+real(dp) :: tol, x(3), fvec(15), fjac(size(fvec), size(x)), eps, err
 integer :: ipvt(size(x))
 real(dp), allocatable :: wa(:)
 
 ! The following starting values provide a rough fit.
 x = [1._dp, 1._dp, 1._dp]
 
-call check_deriv()
+call check_deriv(x, fvec, fjac)
 
 ! Set tol to the square root of the machine precision. Unless high precision
 ! solutions are required, this is the recommended setting.
 tol = sqrt(epsilon(1._dp))
 
 allocate(wa(5*size(x) + size(fvec)))
-call lmder1(fcn, size(fvec), size(x), x, fvec, fjac, size(fjac, 1), tol, &
+call lmder1(fcn2, size(fvec), size(x), x, fvec, fjac, size(fjac, 1), tol, &
     info, ipvt, wa, size(wa))
 print 1000, enorm(size(fvec), fvec), info, x
 1000 format(5x, 'FINAL L2 NORM OF THE RESIDUALS', d15.7 // &
@@ -70,19 +88,21 @@ print 1000, enorm(size(fvec), fvec), info, x
             5x, 'FINAL APPROXIMATE SOLUTION'            // &
             5x, 3d15.7)
 
+eps = 2.2e-16_dp ! epsilon(1._dp)
+print *, "sum(x) = ", sum2(x)
+err = abs(sum2(x) - (3.5591418689884917_dp))
+print *, "sum(x) error: ", err
+if (err > eps) error stop
+
 contains
 
-subroutine check_deriv()
-real(dp) :: xp(size(x)), fvecp(size(fvec)), err(size(fvec))
-call chkder(size(fvec), size(x), x, fvec, fjac, size(fjac, 1), xp, fvecp, &
-    1, err)
-call fcn(size(fvec), size(x), x, fvec, fjac, size(fjac, 1), 1)
-call fcn(size(fvec), size(x), x, fvec, fjac, size(fjac, 1), 2)
-call fcn(size(fvec), size(x), xp, fvecp, fjac, size(fjac, 1), 1)
-call chkder(size(fvec), size(x), x, fvec, fjac, size(fjac, 1), xp, fvecp, &
-    2, err)
-print *, "Derivatives check (1.0 is correct, 0.0 is incorrect):"
-print *, err
-end subroutine
+    real(dp) function sum2(x) result(r)
+    real(dp), intent(in) :: x(:)
+    integer :: i
+    r = 0
+    do i = 1, size(x)
+        r = r + x(i)
+    end do
+    end function
 
 end program
